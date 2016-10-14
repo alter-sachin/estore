05f3449904f2d1d6baee0df9e9533d64180a75af
alter-sachin <guitarplayersachin@gmail.com>
2016-10-14 03:18:04 -0400

    image decorator addition

--- app/models/image_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Image.class_eval do
-  attachment_definitions[:attachment][:styles] = {
-    :mini => '96x96>', # thumbs under image
-    :small => '200x200>', # images on category view
-    :product => '480x480>', # full product image
-    :large => '12000x12000>' # light box image
-  }
-end
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -17,3 +17,12 @@ end
 
 Spree.user_class = "Spree::LegacyUser"
 
+Spree::Image.class_eval do
+  attachment_definitions[:attachment][:styles] = {
+    :mini => '96x96>', # thumbs under image
+    :small => '200x200>', # images on category view
+    :product => '480x480>', # full product image
+    :large => '12000x12000>' # light box image
+  }
+end
+
