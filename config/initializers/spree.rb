206126061de3d43e88a7d8990e278a58b5273d08
alter-sachin <guitarplayersachin@gmail.com>
2016-10-14 03:21:21 -0400

    image decorator addition

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -21,7 +21,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '96x96>', # thumbs under image
     :small => '200x200>', # images on category view
-    :product => '480x480>', # full product image
+    :product => '360x510>', # full product image
     :large => '12000x12000>' # light box image
   }
 end
